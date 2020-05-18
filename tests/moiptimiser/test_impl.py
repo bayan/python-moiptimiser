ab418b7a5d7383a50d5eb7108e037d5520a8bc5e
Bayan Khalili <bayan.net@gmail.com>
2020-05-18 18:23:56 +1000

    Better test

---
+++ tests/moiptimiser/test_impl.py
@@ -0,0 +1,18 @@
+from click.testing import CliRunner
+
+from moiptimiser.impl import MOIPtimiser
+
+def test_2AP05():
+    optimiser = MOIPtimiser.from_lp_file('tests/examples/2AP05.lp')
+    nds = optimiser.find_non_dominated_objective_vectors()
+    assert len(nds) == 10
+    assert (50,24) in nds
+    assert (50,24) in nds
+    assert (39,28) in nds
+    assert (34,29) in nds
+    assert (31,30) in nds
+    assert (30,31) in nds
+    assert (27,36) in nds
+    assert (24,45) in nds
+    assert (23,46) in nds
+    assert (21,55) in nds
