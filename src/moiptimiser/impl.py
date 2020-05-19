ff908f829a1eb0bd03c72a984501c3b521e2559f
Bayan Khalili <bayan.net@gmail.com>
2020-05-19 16:25:55 +1000

    Implemented optimisation from (Ozlen et al., 2014)
    Ozlen, M., Burton, B.A., MacRae, C.A.G., 2014. Multi-Objective Integer Programming: An Improved Recursive Algorithm. J Optim Theory Appl 160, 470–482. https://doi.org/10.1007/s10957-013-0364-y

--- src/moiptimiser/impl.py
+++ src/moiptimiser/impl.py
@@ -5,6 +5,7 @@ class MOIPtimiser:
     def __init__(self, model):
         self.__model = model
         self.__init_upper_bound_constraints()
+        self.__relaxation_cache = {}
 
     def __init_upper_bound_constraints(self):
         self.__objective_constraints = []
@@ -30,24 +31,53 @@ class MOIPtimiser:
         bounds = []
         for i in range(self.__model.NumObj):
             bounds.append(self.__objective_constraints[i].rhs)
-        return bounds
+        return tuple(bounds)
+
+    def __store_relaxation_in_cache(self, depth, bounds, solutions):
+        if depth not in self.__relaxation_cache: self.__relaxation_cache[depth] = {}
+        self.__relaxation_cache[depth][bounds] = solutions
+
+    def __find_feasible_relaxation(self, depth):
+        relaxations_at_depth = self.__relaxation_cache.get(depth)
+        if relaxations_at_depth is not None:
+            bounds = self.__current_bounds()
+            for other_bounds in relaxations_at_depth:
+                if bounds[depth:] != other_bounds and all(( x >= y for x,y in zip(other_bounds, bounds[depth:]) )):
+                    is_feasible = True
+                    nds = relaxations_at_depth[other_bounds]
+                    for solution in nds:
+                        if not all(( x >= y for x,y in zip(bounds[depth:], solution[depth:]) )):
+                            is_feasible = False
+                            break
+                    if is_feasible:
+                        return nds
 
     def __find_non_dominated_objective_vectors(self, depth):
-        if depth == 1:
+        relaxation = self.__find_feasible_relaxation(depth)
+        if relaxation is not None: return relaxation
+
+        elif depth == 1:
             self.__model.optimize()
-            return set() if self.__is_infeasible() else {self.__current_nd()}
+            nds = set() if self.__is_infeasible() else {self.__current_nd()}
+            self.__store_relaxation_in_cache(depth, self.__current_bounds()[depth:], nds)
+            return nds
+
         else:
             nds = set()
-            self.__objective_constraints[depth-1].rhs = gurobipy.GRB.INFINITY
+            new_bound = gurobipy.GRB.INFINITY
             while True:
+                self.__objective_constraints[depth-1].rhs = new_bound
+                self.__model.update()
                 new_nds = self.__find_non_dominated_objective_vectors(depth-1)
-                if len(new_nds) == 0: return nds
+                if len(new_nds) == 0:
+                    self.__store_relaxation_in_cache(depth, self.__current_bounds()[depth:], nds)
+                    return nds
                 nds = nds.union(new_nds)
                 new_bound = max([ nd[depth-1] for nd in new_nds ]) - 1
-                self.__objective_constraints[depth-1].rhs = new_bound
 
     def find_non_dominated_objective_vectors(self):
-        return self.__find_non_dominated_objective_vectors(self.__model.NumObj)
+        nds = self.__find_non_dominated_objective_vectors(self.__model.NumObj)
+        return nds
 
     def from_lp_file(filepath):
         model = gurobipy.read(filepath)
