4c1e96a25111a50faf714a938b3162516f173dd5
Bayan Khalili <bayan.net@gmail.com>
2020-05-19 18:57:44 +1000

    Extract duplicate code into reusable method

--- src/moiptimiser/impl.py
+++ src/moiptimiser/impl.py
@@ -39,16 +39,20 @@ class MOIPtimiser:
             self.__relaxation_cache[depth] = {}
         self.__relaxation_cache[depth][bounds] = solutions
 
+    # True if each left[i] >= right[i]
+    def __all_ge(self, left, right):
+        return all((x >= y for x, y in zip(left, right)))
+
     def __find_feasible_relaxation(self, depth):
         relaxations_at_depth = self.__relaxation_cache.get(depth)
         if relaxations_at_depth is not None:
             bounds = self.__current_bounds()
             for other_bounds in relaxations_at_depth:
-                if bounds[depth:] != other_bounds and all((x >= y for x, y in zip(other_bounds, bounds[depth:]))):
+                if bounds[depth:] != other_bounds and self.__all_ge(other_bounds, bounds[depth:]):
                     is_feasible = True
                     nds = relaxations_at_depth[other_bounds]
                     for solution in nds:
-                        if not all((x >= y for x, y in zip(bounds[depth:], solution[depth:]))):
+                        if not self.__all_ge(bounds[depth:], solution[depth:]):
                             is_feasible = False
                             break
                     if is_feasible:
