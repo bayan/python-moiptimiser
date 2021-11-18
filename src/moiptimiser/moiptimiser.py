1a796067c147a10b41900cd202b9ad9419a79401
Bayan Khalili <bayan.net@gmail.com>
2021-11-18 19:41:52 +1100

    Move more redundant code into base class and use protected convention rather than private mangled convention

--- src/moiptimiser/moiptimiser.py
+++ src/moiptimiser/moiptimiser.py
@@ -2,6 +2,23 @@ import gurobipy
 
 class MOIPtimiser:
 
+    def __init__(self, model):
+        self._model = model
+        self._init_bound_constraints()
+
+    def _init_bound_constraints(self):
+        self._objective_constraints = []
+        for i in range(self._model.NumObj):
+            objective = self._model.getObjective(i)
+            if self._is_min():
+                constraint = self._model.addLConstr(objective, '<', gurobipy.GRB.INFINITY)
+            else:
+                constraint = self._model.addLConstr(objective, '>', -gurobipy.GRB.INFINITY)
+            self._objective_constraints.append(constraint)
+
+    def _is_min(self):
+        return self._model.ModelSense == 1
+
     @classmethod
     def from_lp_file(cls, filepath):
         model = gurobipy.read(filepath)
--- src/moiptimiser/tamby_2020_moiptimiser.py
+++ src/moiptimiser/tamby_2020_moiptimiser.py
@@ -1,32 +1,14 @@
-import gurobipy
-from moiptimiser.moiptimiser import MOIPtimiser
+from moiptimiser.moiptimiser import *
 
 class Tamby2020MOIPtimiser(MOIPtimiser):
 
-    def __init__(self, model):
-        self.__model = model
-        self.__init_bound_constraints()
-
-    def __init_bound_constraints(self):
-        self.__objective_constraints = []
-        for i in range(self.__model.NumObj):
-            objective = self.__model.getObjective(i)
-            if self.__is_min():
-                constraint = self.__model.addLConstr(objective, '<', gurobipy.GRB.INFINITY)
-            else:
-                constraint = self.__model.addLConstr(objective, '>', -gurobipy.GRB.INFINITY)
-            self.__objective_constraints.append(constraint)
-
-    def __is_min(self):
-        return self.__model.ModelSense == 1
-
-    def __update_search_region(self):
+    def _update_search_region(self):
         pass
 
-    def __find_non_dominated_objective_vectors(self):
+    def _find_non_dominated_objective_vectors(self):
         nds = set()
         return nds
 
     def find_non_dominated_objective_vectors(self):
-        nds = self.__find_non_dominated_objective_vectors()
+        nds = self._find_non_dominated_objective_vectors()
         return nds
