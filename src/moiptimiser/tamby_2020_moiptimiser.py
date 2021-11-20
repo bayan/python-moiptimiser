36f9904ea650b00aca46a3c7bfbd9d85e3b8a229
Bayan Khalili <bayan.net@gmail.com>
2021-11-20 21:12:31 +1100

    Start implementing the rest of the (Tamby & Vanderpooten, 2020) algorithm

--- src/moiptimiser/tamby_2020_moiptimiser.py
+++ src/moiptimiser/tamby_2020_moiptimiser.py
@@ -4,20 +4,68 @@ class Tamby2020MOIPtimiser(MOIPtimiser):
 
     def __init__(self, model):
         super().__init__(model)
+        self._search_region = set()
+        self._defining_points = dict()
+        self._init_M()
         self._init_ideal_point()
 
-    def _update_search_region(self):
-        pass
+    def _init_M(self):
+        self._M = gurobipy.GRB.MAXINT
 
-    def _find_non_dominated_objective_vectors(self):
-        nds = set()
-        return nds
 
     def _kth_projection(self, point, k):
         return tuple(list(point[0:k]) + list(point[k+1:len(point)]))
 
     def strictly_dominates(self, left, right):
         return all((x < y for x, y in zip(left, right)))
+
+    # Algorithm 1
+    def _update_search_region(self, new_point):
+        # Output
+        new_search_region = self._search_region.copy()
+        # Line 2
+        for u in self._search_region:
+            # Line 3
+            if self.strictly_dominates(new_point,u):
+                # Line 4
+                new_search_region.remove(u)
+                # Line 5
+                for l in range(self._model.NumObj):
+                    # Line 6
+                    ul = u.copy()
+                    ul[l] = new_point[l]
+                    ul = tuple(ul)
+                    # Line 7
+                    for k in range(self._model.NumObj):
+                        new_defining_points = set()
+                        if k != l:
+                            # Line 8
+                            for defining_point in self._defining_points[(k, u)]:
+                                if defining_point[l] < new_point[l]:
+                                    new_defining_points.add(defining_point)
+                        self._defining_points[(k, ul)] = new_defining_points
+                    # Line 9
+                    self._defining_points[(l,ul)] = set([new_point])
+                    # Line 10
+                    valid_defining_point_exists = True
+                    for k in range(self._model.NumObj):
+                        if k != l:
+                            if ul[k] != self._M:
+                                if len(self._defining_points[(k, ul)]) == 0:
+                                    valid_defining_point_exists = False
+                                    break
+                    if valid_defining_point_exists:
+                        # Line 11
+                        new_search_region.add(ul)
+            # Line 12
+            else:
+                for k in range(self._model.NumObj):
+                    if k != l:
+                        if new_point[k] == u[k]:
+                            kth_new_point_projection = self._kth_projection(new_point, k)
+                            kth_u_projection = self._kth_projection(u, k)
+                            if self.strictly_dominates(kth_new_point_projection, kth_u_projection):
+                                self._defining_points[(k,u)].add(new_point)
 
     def _copy_vars_to(self, source, target):
         for var in source.getVars():
@@ -63,6 +111,27 @@ class Tamby2020MOIPtimiser(MOIPtimiser):
             kth_model.optimize()
             point.append(kth_model.ObjNVal)
         self._ideal_point = tuple(point)
+
+    # Algorithm 2
     def find_non_dominated_objective_vectors(self):
-        nds = self._find_non_dominated_objective_vectors()
-        return nds
+        # Output
+        Y_ND = set()
+
+        # Line 1
+        N = set()
+        U = set([self._M] * self._model.NumObj)
+        V = {}
+        for k in range(self._model.NumObj):
+            V[k] = set()
+
+        # Line 2
+        # Ideal point already computed on class instantiation.
+
+        # Line 3
+        while len(U) > 0:
+
+            # Line 4
+            pass
+
+        # Output
+        return Y_ND
