045edc69bda612f7d9430dc5e8ac313707fd2747
Bayan Khalili <bayan.net@gmail.com>
2021-12-14 06:02:08 +1100

    Bump version: 0.0.11 → 0.0.12

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'moiptimiser'
 year = '2020'
 author = 'Bayan Khalili'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.11'
+version = release = '0.0.12'
 
 pygments_style = 'trac'
 templates_path = ['.']
